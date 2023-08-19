9bf4e514e12b4b3287afabb5119077f10a8f4ccf
Haralds <129745067+Hariic33@users.noreply.github.com>
2023-08-19 21:37:43 +0300

    modified quiz taking functionality (#7)

--- app/controllers/quiz_taking_controller.rb
+++ app/controllers/quiz_taking_controller.rb
@@ -1,38 +1,46 @@
 class QuizTakingController < ApplicationController
+  before_action :load_quiz_and_questions, only: [:take, :submit]
+  
   def take
-    @quiz = Quiz.find(params[:id])
-    @questions = @quiz.questions
-    @user_answers = {}
+    @current_question = @questions[@current_question_index]
   end
 
   def submit
-    @quiz = Quiz.find(params[:id])
-    @questions = @quiz.questions
-    @user_answers = params[:user_answers]
-    @score = 0
-    all_fields_filled = true
+    user_answer = params[:user_answer]
+    save_user_answer(@current_question, user_answer)
 
-    @questions.each do |question|
-      user_answer = @user_answers["q#{question.id}_answer"]
-      if user_answer == question.answer
-        @score += 1
-      elsif user_answer.blank?
-        all_fields_filled = false
-        flash[:alert] = 'Please answer all questions before submitting.'
-        break
-      end
-    end
-
-    if all_fields_filled
-      session[:quiz_score] = @score
+    if user_answer.blank?
+      flash[:alert] = 'Please enter an answer before submitting.'
+      redirect_to take_quiz_path(@quiz, current_question_index: @current_question_index)
+    elsif @current_question_index == @questions.length - 1
+      session[:quiz_score] = calculate_score
       redirect_to quiz_results_path(@quiz)
     else
-      render :take
+      redirect_to take_quiz_path(@quiz, current_question_index: @current_question_index + 1)
     end
   end
 
   def results
     @quiz = Quiz.find(params[:id])
     @score = session[:quiz_score]
+  end
+
+  private
+
+  def load_quiz_and_questions
+    @quiz = Quiz.find(params[:id])
+    @questions = @quiz.questions
+    @current_question_index = params[:current_question_index].to_i
+    @current_question = @questions[@current_question_index]
+  end
+
+  def save_user_answer(question, answer)
+    session["quiz_#{params[:id]}_user_answers"] ||= {}
+    session["quiz_#{params[:id]}_user_answers"]["q#{question.id}_answer"] = answer
+  end
+
+  def calculate_score
+    user_answers = session["quiz_#{params[:id]}_user_answers"]
+    @questions.count { |question| user_answers["q#{question.id}_answer"] == question.answer }
   end
 end
