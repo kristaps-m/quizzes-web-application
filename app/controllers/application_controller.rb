b4ee936bb71d28badeb778525acf1a91c790a1c7
Kristaps Mitins <kristapsmitins@gmail.com>
2023-08-22 05:02:30 -0400

    Now User is able to create new profile and edit it

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,9 @@ class ApplicationController < ActionController::Base
 
     private
   
+    # This private method is for new User fields [name, lastname] (create, edit)
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
     end
   end
