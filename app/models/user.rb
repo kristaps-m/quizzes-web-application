cb86c635335819b6ef11767fce1437096063c4b7
Kristaps Mitins <61430801+kristaps-m@users.noreply.github.com>
2023-08-25 20:34:50 +0300

    Update user.rb
    add few lines of code `, dependent: :destroy`
    Now it is possible to delete User, associated quizzes, and statistics.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :quizzes, dependent: :destroy
-  has_many :quiz_statistics
+  has_many :quiz_statistics, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
