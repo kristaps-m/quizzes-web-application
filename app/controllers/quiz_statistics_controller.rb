3996c40d988fe7c8a12b95e9a67ce913a2462d89
Hariic33 <hmakbets@gmail.com>
2023-08-25 05:32:35 -0400

    finishing touches in quiz statistics and css

--- app/controllers/quiz_statistics_controller.rb
+++ app/controllers/quiz_statistics_controller.rb
@@ -5,19 +5,12 @@ class QuizStatisticsController < ApplicationController
     @quiz_statistics = current_user.quiz_statistics
   end
 
-  # def new
-  #   @quiz_statistic = QuizStatistic.new
-  # end
-
   def show
   end
 
   def create
-    # @quiz_statistic = current_user.quiz_statistics.new(quiz_statistic_params)
     @quiz_statistic = QuizStatistic.new(quiz_statistic_params)
     if @quiz_statistic.save
-      # flash.notice = "Quiz statistic saved successfully!"
-      # redirect_to quiz_statistics_path
       redirect_to @quiz_statistic, notice: 'Quiz statistic saved successfully!'
     else
       flash.now.alert = "Failed to save quiz statistic"
